c9bc8ce256f514ef39767f6130f8d2cac96c7576
Yusufu U Kamara <59753837+yukamara@users.noreply.github.com>
2021-08-28 00:58:38 -0700

    Updated html code and flask app code

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 #import Flask
 from joblib import load
 from flask import Flask, render_template, request, redirect, url_for
-from flask import Flask, render_template
+import pickle
+
+
 # create an instance of Flask
 app = Flask(__name__)
 
+
 # load the pipeline object
-#pipeline = load("model_name.joblib")
+#model = pickle.load(open('amazon_reviews.pkl', 'rb'))
+pipeline = load("amazon_reviews.joblib")
 
 
+# create route that renders index.html template
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+# Create a route to get reviews and return ratings for the review
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        name = request.form["amazonReview"]
+
+        review = pipeline.predict(name)
+
+    return render_template("form.html", review_text='This review is rated {}.')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
