c00f1f12008a2c27f8380235c8aef16713075792
Yusufu U Kamara <59753837+yukamara@users.noreply.github.com>
2021-08-27 17:40:29 -0700

    Updated pandas ML model, update html code, saved ML model as .joblib & .py files

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
